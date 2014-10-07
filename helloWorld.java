0177ad42a3d905e42d22a65ad6e08b3bb08c9bd9
antonh <antonh13@ru.is>
2014-10-07 17:02:36 +0000

    Message til Nonna

--- helloWorld.java
+++ helloWorld.java
@@ -4,5 +4,6 @@ public class helloWorld
   public static void main(String[] args)
   {
     System.out.println("Matrix er best saettu thig vid that!");
+    System.out.println("Ekki sjens Jon Helgi Jonsson!")
   }
 }
