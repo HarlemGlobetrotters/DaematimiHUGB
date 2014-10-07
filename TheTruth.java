c8eaaee3c3aeb874b1abba95ceea012524809342
jonj12 <jonj12@ru.is>
2014-10-07 16:57:37 +0000

    nonni

---
+++ TheTruth.java
@@ -0,0 +1,7 @@
+public class helloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Matrix er best saettu thig vid that!");
+  }
+}
---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+
+public class helloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Matrix er best saettu thig vid that!");
+  }
+}
