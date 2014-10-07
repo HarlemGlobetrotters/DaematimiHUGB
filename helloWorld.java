c29a3246de90d96e927c20839212292bf41cdbb1
antonh <antonh13@ru.is>
2014-10-07 17:05:54 +0000

    Message

--- helloWorld.java
+++ helloWorld.java
@@ -4,6 +4,6 @@ public class helloWorld
   public static void main(String[] args)
   {
     System.out.println("Matrix er best saettu thig vid that!");
-    System.out.println("Ekki sjens Jon Helgi Jonsson!")
+    System.out.println("Ekki sjens Jon Helgi Jonsson!");
   }
 }
